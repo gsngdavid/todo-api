20b7c8aeb51890515fa05b95096d84de96fd1609
gsngdavid <gsngdavid@gmail.com>
2023-12-14 16:06:40 +0200

    feat: test categoriesService

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -4,10 +4,21 @@ import { CategoriesRepository } from './categories.repository';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let repo: CategoriesRepository;
+  const mockCategories = [{ id: '1', name: 'work' }];
 
   beforeEach(async () => {
     const fakeCategoriesRepository: Partial<CategoriesRepository> = {
-      getCategories: () => Promise.resolve([]),
+      getCategories: jest.fn().mockResolvedValueOnce(mockCategories),
+      createCategory: jest
+        .fn()
+        .mockImplementation(
+          (category: { id: string; name: string } = mockCategories[0]) =>
+            Promise.resolve({
+              id: category.id,
+              message: 'success',
+            }),
+        ),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -21,14 +32,34 @@ describe('CategoriesService', () => {
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
+    repo = module.get<CategoriesRepository>(CategoriesRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return an array', async () => {
-    const categories = await service.getCategories();
-    expect(categories).toBeInstanceOf(Array);
+  describe('getCategories', () => {
+    it('should get all categories', async () => {
+      const categories = await service.getCategories();
+      expect(repo.getCategories).toHaveBeenCalled();
+      expect(categories).toEqual(mockCategories);
+    });
+  });
+
+  describe('createCategory', () => {
+    it('should throw an error if the category already exists', (done) => {
+      service
+        .createCategory('work')
+        .then()
+        .catch(() => done());
+    });
+
+    it('should return a success object when category is successfully created', async () => {
+      const result = await service.createCategory('Personal');
+      expect(repo.createCategory).toHaveBeenCalled();
+      expect(result.id).toBeDefined();
+      expect(result.message).toBeDefined();
+    });
   });
 });
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,9 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CategoriesRepository } from './categories.repository';
 import { v4 as uuidv4 } from 'uuid';
-import db from '../db';
 import { CategoryEntity } from '../types/interfaces';
-import { DataError } from 'node-json-db';
 
 @Injectable()
 export class CategoriesService {
@@ -14,15 +12,11 @@ export class CategoriesService {
   }
 
   async createCategory(name: string) {
-    let categories: CategoryEntity[];
-    try {
-      categories = await db.getData('/categories');
-    } catch (error) {
-      if (error instanceof DataError) categories = [];
-    }
+    const categories: CategoryEntity[] = await this.getCategories();
 
-    if (categories.some((category) => category.name === name))
+    if (categories.some((category) => category.name === name)) {
       throw new BadRequestException('Category already exists');
+    }
 
     const id = uuidv4();
     return this.repo.createCategory({ id, name });
